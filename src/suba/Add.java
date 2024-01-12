3f3e114c5e3080ba4574da951fa3681ab5e79acb
Elitebook 840 G6 <Elitebook 840 G6@suba>
2024-01-12 11:52:07 +0530

    first commit

---
+++ src/suba/Add.java
@@ -0,0 +1,13 @@
+package suba;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a=5;    //  create variable
+		int b=6;     // create another variable
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
